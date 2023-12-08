63a83512cbfc1075f71157ff656d62295edf3bff
Flynn Duniho <flynn@harveyosity.org>
2023-12-07 19:54:51 -0500

    got animation working

---
+++ README.md
@@ -0,0 +1,4 @@
+# WindowAnim
+---
+
+An experiment playing around with spawning and animating windows in Python. Inspired by the game [Windowkill](https://torcado.itch.io/windowkill)
---
+++ anim.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import time
+import math
+start_time = time.time_ns()
+
+window = tk.Tk()
+
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+window.resizable(False, False)
+window.attributes('-topmost', 1)
+window.overrideredirect(True)
+window.title("Anim")
+window.configure(bg="white")
+
+def gettime() -> float:
+    return (time.time_ns() - start_time) / 1000000000
+
+def setpos(win: tk.Tk, x: float, y: float, w: float, h: float):
+    string = "{}x{}+{}+{}".format(int(w), int(h), int(x), int(y))
+    win.geometry(string) 
+
+while True:
+    t = gettime()
+    w = math.sin(t * 2 * math.pi * 1) * 50 + 100
+    h = math.cos(t * 2 * math.pi * 1) * 50 + 100
+    x = math.cos(t * 2 * math.pi * 0.5) * 100 + screen_width / 2 - w / 2
+    y = math.sin(t * 2 * math.pi * 0.5) * 100 + screen_height / 2 - h / 2
+    setpos(window, x, y, w, h)
+    window.update_idletasks()
+    window.update()
+# math.cos(t / 2 / math.pi / 20) * 50 + 
+# math.sin(t / 2 / math.pi / 20) * 50 + 
